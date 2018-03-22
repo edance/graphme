dc07f4b9da51990470f8b067098994086afbf70c
Evan Dancer <evancdancer@gmail.com>
2018-03-22 09:49:36 -0700

    Getting started with graph

---
+++ api/controllers/GraphController.js
@@ -0,0 +1,11 @@
+/**
+ * GraphController
+ *
+ * @description :: Server-side logic for managing graphs
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
